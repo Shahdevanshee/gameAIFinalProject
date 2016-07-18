52b4f89f3140d8ba53b4964c5f2eb68c5a28fd2d
ihbuckl <ihbuckl@g.clemson.edu>
2016-07-18 17:59:49 -0400

    re-implemented movement, respawns player, added more behaviours to choose from

--- astarnavigator.py
+++ astarnavigator.py
@@ -1,3 +1,21 @@
+'''
+ * Copyright (c) 2014, 2015 Entertainment Intelligence Lab, Georgia Institute of Technology.
+ * Originally developed by Mark Riedl.
+ * Last edited by Mark Riedl 05/2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+'''
+
 import sys, pygame, math, numpy, random, time, copy
 from pygame.locals import * 
 
@@ -12,158 +30,169 @@ from mynavigatorhelpers import *
 ### AStarNavigator
 ###
 ### Creates a path node network and implements the FloydWarshall all-pairs shortest-path algorithm to create a path to the given destination.
-			
+            
 class AStarNavigator(NavMeshNavigator):
 
-	def __init__(self):
-		NavMeshNavigator.__init__(self)
-		
+    def __init__(self):
+        NavMeshNavigator.__init__(self)
+        
 
-	### Create the pathnode network and pre-compute all shortest paths along the network.
-	### self: the navigator object
-	### world: the world object
-	def createPathNetwork(self, world):
-		self.pathnodes, self.pathnetwork, self.navmesh = myCreatePathNetwork(world, self.agent)
-		return None
-		
-	### Finds the shortest path from the source to the destination using A*.
-	### self: the navigator object
-	### source: the place the agent is starting from (i.e., it's current location)
-	### dest: the place the agent is told to go to
-	def computePath(self, source, dest):
-		### Make sure the next and dist matricies exist
-		if self.agent != None and self.world != None: 
-			self.source = source
-			self.destination = dest
-			### Step 1: If the agent has a clear path from the source to dest, then go straight there.
-			###   Determine if there are no obstacles between source and destination (hint: cast rays against world.getLines(), check for clearance).
-			###   Tell the agent to move to dest
-			### Step 2: If there is an obstacle, create the path that will move around the obstacles.
-			###   Find the pathnodes closest to source and destination.
-			###   Create the path by traversing the self.next matrix until the pathnode closes to the destination is reached
-			###   Store the path by calling self.setPath()
-			###   Tell the agent to move to the first node in the path (and pop the first node off the path)
-			if clearShot(source, dest, self.world.getLines(), self.world.getPoints(), self.agent):
-				self.agent.moveToTarget(dest)
-			else:
-				start = findClosestUnobstructed(source, self.pathnodes, self.world.getLinesWithoutBorders())
-				end = findClosestUnobstructed(dest, self.pathnodes, self.world.getLinesWithoutBorders())
-				if start != None and end != None:
-					print len(self.pathnetwork)
-					newnetwork = unobstructedNetwork(self.pathnetwork, self.world.getGates())
-					print len(newnetwork)
-					print "ok"
-					closedlist = []
-					path, closedlist = astar(start, end, newnetwork)
-					if path is not None and len(path) > 0:
-						path = shortcutPath(source, dest, path, self.world, self.agent)
-						self.setPath(path)
-						if self.path is not None and len(self.path) > 0:
-							first = self.path.pop(0)
-							self.agent.moveToTarget(first)
-		return None
-		
-	### Called when the agent gets to a node in the path.
-	### self: the navigator object
-	def checkpoint(self):
-		myCheckpoint(self)
-		return None
+    ### Create the pathnode network and pre-compute all shortest paths along the network.
+    ### self: the navigator object
+    ### world: the world object
+    def createPathNetwork(self, world):
+        self.pathnodes, self.pathnetwork, self.navmesh = myCreatePathNetwork(world, self.agent)
+        return None
+        
+    ### Finds the shortest path from the source to the destination using A*.
+    ### self: the navigator object
+    ### source: the place the agent is starting from (i.e., it's current location)
+    ### dest: the place the agent is told to go to
+    def computePath(self, source, dest):
+        ### Make sure the next and dist matricies exist
+        if self.agent != None and self.world != None: 
+            self.source = source
+            self.destination = dest
+            ### Step 1: If the agent has a clear path from the source to dest, then go straight there.
+            ###   Determine if there are no obstacles between source and destination (hint: cast rays against world.getLines(), check for clearance).
+            ###   Tell the agent to move to dest
+            ### Step 2: If there is an obstacle, create the path that will move around the obstacles.
+            ###   Find the pathnodes closest to source and destination.
+            ###   Create the path by traversing the self.next matrix until the pathnode closes to the destination is reached
+            ###   Store the path by calling self.setPath()
+            ###   Tell the agent to move to the first node in the path (and pop the first node off the path)
+            if clearShot(source, dest, self.world.getLines(), self.world.getPoints(), self.agent):
+                self.agent.moveToTarget(dest)
+            else:
+                start = findClosestUnobstructed(source, self.pathnodes, self.world.getLinesWithoutBorders())
+                end = findClosestUnobstructed(dest, self.pathnodes, self.world.getLinesWithoutBorders())
+                if start != None and end != None:
+                    #print len(self.pathnetwork)
+                    newnetwork = unobstructedNetwork(self.pathnetwork, self.world.getGates())
+                    #print len(newnetwork)
+                    closedlist = []
+                    path, closedlist = astar(start, end, newnetwork)
+                    if path is not None and len(path) > 0:
+                        path = shortcutPath(source, dest, path, self.world, self.agent)
+                        self.setPath(path)
+                        if self.path is not None and len(self.path) > 0:
+                            first = self.path.pop(0)
+                            self.agent.moveToTarget(first)
+        return None
+        
+    ### Called when the agent gets to a node in the path.
+    ### self: the navigator object
+    def checkpoint(self):
+        myCheckpoint(self)
+        return None
 
-	### This function gets called by the agent to figure out if some shortcutes can be taken when traversing the path.
-	### This function should update the path and return True if the path was updated.
-	def smooth(self):
-		return mySmooth(self)
+    ### This function gets called by the agent to figure out if some shortcuts can be taken when traversing the path.
+    ### This function should update the path and return True if the path was updated.
+    def smooth(self):
+        return mySmooth(self)
 
-	def update(self, delta):
-		myUpdate(self, delta)
+    def update(self, delta):
+        myUpdate(self, delta)
 
 
 def unobstructedNetwork(network, worldLines):
-	newnetwork = []
-	for l in network:
-		hit = rayTraceWorld(l[0], l[1], worldLines)
-		if hit == None:
-			newnetwork.append(l)
-	return newnetwork
-
-
-def insert(x, list, func = lambda x: x):
-	for i in xrange(len(list)):
-		if func(x) < func(list[i]):
-			list.insert(i, x)
-			return list
-	list.append(x)
-	return list
-	
+    newnetwork = []
+    for l in network:
+        hit = rayTraceWorld(l[0], l[1], worldLines)
+        if hit == None:
+            newnetwork.append(l)
+    return newnetwork
 
 def astar(init, goal, network):
-	path = []
-	open = []
-	closed = []
-	### YOUR CODE GOES BELOW HERE ###
-	init =(init ,0 ,distance (init ,goal ),None )#line:3
-	OO000O0000O0000O0 =set ()#line:4
-	OO0O000O00O0O000O =set ()#line:5
-	O0000O0O00O00O0O0 =[init ]#line:6
-	O000OOOO0OOOOOO00 =init #line:7
-	while O000OOOO0OOOOOO00 is not None and O000OOOO0OOOOOO00 [0 ]!=goal and len (O0000O0O00O00O0O0 )>0 :#line:10
-		OO000O0000O0000O0 .add (O000OOOO0OOOOOO00 [0 ])#line:11
-		OO0O000O00O0O000O .add (O000OOOO0OOOOOO00 )#line:12
-		O0000O0O00O00O0O0 .pop (0 )#line:13
-		OOOOOOO0000000OO0 =foow (O000OOOO0OOOOOO00 ,network ,goal )#line:15
-		for O0OOO0OO00OOOO0O0 in OOOOOOO0000000OO0 :#line:17
-			if O0OOO0OO00OOOO0O0 [0 ]not in OO000O0000O0000O0 :#line:18
-				insert (O0OOO0OO00OOOO0O0 ,O0000O0O00O00O0O0 ,lambda x :x [1 ]+x [2 ])#line:19
-		if len (O0000O0O00O00O0O0 )>0 :#line:21
-			O000OOOO0OOOOOO00 =O0000O0O00O00O0O0 [0 ]#line:22
-		else :#line:23
-			O000OOOO0OOOOOO00 =None #line:24
-	if O000OOOO0OOOOOO00 is not None :#line:27
-		while O000OOOO0OOOOOO00 [3 ]is not None :#line:28
-			path .append (O000OOOO0OOOOOO00 [0 ])#line:29
-			OO0O0000O00OO00OO =O000OOOO0OOOOOO00 [3 ]#line:30
-			for OOOO00O00OOOOO0O0 in list (OO0O000O00O0O000O ):#line:31
-				if OO0O0000O00OO00OO ==OOOO00O00OOOOO0O0 [0 ]:#line:32
-					O000OOOO0OOOOOO00 =OOOO00O00OOOOO0O0 #line:33
-					break #line:34
-		path .append (O000OOOO0OOOOOO00 [0 ])#line:35
-		path .reverse ()#line:36
-	OO000O0000O0000O0 =list (OO000O0000O0000O0 )	
-	### YOUR CODE GOES ABOVE HERE ###
-	return path, closed
-	
-	
-def foow (node ,network ,goal ):#line:1
-	O00O00O00OO00O0OO =[]#line:2
-	for O0000OO0OO0OOOO0O in network :#line:3
-		if O0000OO0OO0OOOO0O [0 ]==node [0 ]:#line:4
-			O00O00O00OO00O0OO .append ((O0000OO0OO0OOOO0O [1 ],node [1 ]+distance (O0000OO0OO0OOOO0O [0 ],O0000OO0OO0OOOO0O [1 ]),distance (O0000OO0OO0OOOO0O [1 ],goal ),node [0 ]))#line:5
-		elif O0000OO0OO0OOOO0O [1 ]==node [0 ]:#line:6
-			O00O00O00OO00O0OO .append ((O0000OO0OO0OOOO0O [0 ],node [1 ]+distance (O0000OO0OO0OOOO0O [0 ],O0000OO0OO0OOOO0O [1 ]),distance (O0000OO0OO0OOOO0O [0 ],goal ),node [0 ]))#line:7
-	return O00O00O00OO00O0OO 
-	
+    path = []
+    openSet = []
+    closedSet = []
+    cameFrom = {}
+
+    # Get Nodes
+    nodes=list(set([node for edge in network for node in edge])) 
+
+    # Initialize Cost Map    
+    gScore={}
+    fScore={}
+    for node in nodes:
+        gScore[node]=float('Inf')
+        fScore[node]=float('Inf')
+    gScore[init]=0
+    fScore[init]=0
+
+    # Search
+    openSet.append(init)
+    while openSet:
+        
+        minF=float('Inf')
+        for node in openSet:
+            if fScore[node]<minF:
+                current=node
+                minF=fScore[node]
+
+        if current==goal:
+            path=reconstruct_path(cameFrom,current)
+            break
+
+        openSet.remove(current)
+        closedSet.append(current)
+        neighbours=get_neighbours(current,nodes,network)
+        
+        for neighbour in neighbours:
+            if neighbour in closedSet:
+                continue
+            tentative_gScore=gScore[current]+mydistance(current, neighbour)
+            if neighbour not in openSet:
+                openSet.append(neighbour)
+            elif tentative_gScore >= gScore[neighbour]:
+                continue
+            cameFrom[neighbour]=current
+            gScore[neighbour]=tentative_gScore
+            fScore[neighbour]=gScore[neighbour]+mydistance(neighbour,goal)
+    #drawAstarPath(path,)   
+    return path[::-1], closedSet
+
+def mydistance(p1,p2):
+    return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+
+#TODO world surface?
+def drawAstarPath(path, surface):
+	if path is not None:
+		for i in range(0,len(path)-1):
+			pygame.draw.line(surface, (0, 0, 255), i, i+1, 2)
+
+def reconstruct_path(cameFrom,current):
+    fullPath=[current]
+    while current in cameFrom.keys():
+        current=cameFrom[current]
+        fullPath.append(current)
+    return fullPath    
+
+def get_neighbours(node,Nodes,network):
+    neighbours=[]
+    for node2 in Nodes:
+        if ((node,node2) in network or (node2,node) in network) and node2 not in neighbours:
+            neighbours.append(node2)
+    return neighbours
 
 def myUpdate(nav, delta):
-	### YOUR CODE GOES BELOW HERE ###
-	if nav .getPath ()is not None :#line:3
-		OOOOO0OOOOO0O00OO =nav .world .getGates ()#line:4
-		O0OO0000O0OO0O0O0 =nav .agent .getLocation ()#line:14
-		for O00OO000OO00O0O0O in nav .getPath ()+[nav .getDestination ()]:#line:15
-			if O0OO0000O0OO0O0O0 is not None :#line:16
-				OO0O0O00O0OOOOOOO =rayTraceWorld (O0OO0000O0OO0O0O0 ,O00OO000OO00O0O0O ,OOOOO0OOOOO0O00OO )#line:17
-				if OO0O0O00O0OOOOOOO is not None :#line:18
-					nav .setPath (None )#line:20
-					nav .agent .stopMoving ()#line:21
-					return None #line:22
-			O0OO0000O0OO0O0O0 =O00OO000OO00O0O0O #line:23		
-	### YOUR CODE GOES ABOVE HERE ###
-	return None
+    ### YOUR CODE GOES BELOW HERE ###
+    if not clearShot(nav.agent.position, nav.agent.getMoveTarget(), nav.world.getLines(), nav.world.getPoints(), nav.agent):
+        nav.agent.stopMoving()
+        nav.agent.moveToTarget(findClosestUnobstructed(nav.agent.position, nav.pathnodes, nav.world.getLines()))
+    ### YOUR CODE GOES ABOVE HERE ###
+    return None
 
 def myCheckpoint(nav):
-	### YOUR CODE GOES BELOW HERE ###
+    ### YOUR CODE GOES BELOW HERE ###
+    for i in range(0,len(nav.path[:])-1):
+        if not clearShot(nav.path[i], nav.path[i+1], nav.world.getLines(), nav.world.getPoints(), nav.agent):
+            nav.computePath(nav.agent.position, nav.destination)
+            return None
+    ### YOUR CODE GOES ABOVE HERE ###
+    return None
 
-	### YOUR CODE GOES ABOVE HERE ###
-	return None
 
 ### Returns true if the agent can get from p1 to p2 directly without running into an obstacle.
 ### p1: the current location of the agent
@@ -171,20 +200,13 @@ def myCheckpoint(nav):
 ### worldLines: all the lines in the world
 ### agent: the Agent object
 def clearShot(p1, p2, worldLines, worldPoints, agent):
-	### YOUR CODE GOES BELOW HERE ###
-	operational_radius =agent .getRadius ()*2.0 #line:3
-	intersection =rayTraceWorld (p1 ,p2 ,worldLines )#line:4
-	if intersection ==None :#line:5
-		is_clear_shot =False #line:6
-		for point in worldPoints :#line:7
-			if minimumDistance ((p1 ,p2 ),point )<operational_radius :#line:8
-				is_clear_shot =True #line:9
-		if not is_clear_shot :#line:10
-			return True #line:11
-	### YOUR CODE GOES ABOVE HERE ###
-	return False
+    ### YOUR CODE GOES BELOW HERE ###
+    z=[] 
+    radius=(2**1/2)*agent.getMaxRadius()
+    for point in worldPoints:
+        z.append(minimumDistance((p1,p2),point))
+    if rayTraceWorld(p1,p2,worldLines)==None and min(z)>radius:
+        return True
+    ### YOUR CODE GOES ABOVE HERE ###
+    return False
 
-
-#===============================================================#
-# Obfuscated by Oxyry Python Obfuscator (http://pyob.oxyry.com) #
-#===============================================================#
--- mynavigatorhelpers.py
+++ mynavigatorhelpers.py
@@ -22,6 +22,7 @@ from pygame.locals import *
 from constants import *
 from utils import *
 from core import *
+import astarnavigator
 
 ### This function optimizes the given path and returns a new path
 ### source: the current position of the agent
@@ -29,20 +30,82 @@ from core import *
 ### path: the path previously computed by the Floyd-Warshall algorithm
 ### world: pointer to the world
 def shortcutPath(source, dest, path, world, agent):
-	### YOUR CODE GOES BELOW HERE ###
+    ### YOUR CODE GOES BELOW HERE ###
+    edges=[]
+    newpath=[source]+path+[dest]
+    for node1 in newpath:
+        for node2 in newpath:
+            if node1!=node2 and ClearShot(node1, node2, world.getLines(), world.getPoints(), agent) and (node1,node2) not in edges and (node2,node1) not in edges:
+                edges.append((node1,node2))
+    Apath,closedset=astarnavigator.astar(newpath[0], newpath[-1], edges)
+    ### YOUR CODE GOES BELOW HERE ###
+    return Apath
+    #next,dist=APSP(newpath,edges)
+    #return findPath(newpath[0],newpath[-1],next)
 
-	### YOUR CODE GOES BELOW HERE ###
-	return path
+def findPath(start, end, next):
+    path = []
+    if next[start][end]==None:
+        return path
+    path=[start]
+    while start!=end:
+        start=next[start][end]
+        path.append(start)
+    return path
 
+def APSP(nodes, edges):
+    dist = {}
+    next = {}
+    for n in nodes:
+        next[n] = {}
+        dist[n] = {}
+    for node1 in nodes:
+        for node2 in nodes:
+            next[node1][node2]=None
+            dist[node1][node2]=float('Inf')    
+    for node in nodes:
+        next[node][node]=node
+        dist[node][node]=0
+    for edge in edges:
+        dist[edge[0]][edge[1]]=distance(edge[0],edge[1])
+        next[edge[0]][edge[1]]=edge[1]
+        dist[edge[1]][edge[0]]=distance(edge[1],edge[0])
+        next[edge[1]][edge[0]]=edge[0]
+    for node1 in nodes:
+        for node2 in nodes:
+            for node3 in nodes:
+                if dist[node2][node3]>(dist[node2][node1]+dist[node1][node3]):
+                    dist[node2][node3]=(dist[node2][node1]+dist[node1][node3])
+                    next[node2][node3]=next[node2][node1]
+    return next, dist
 
+           
 ### This function changes the move target of the agent if there is an opportunity to walk a shorter path.
 ### This function should call nav.agent.moveToTarget() if an opportunity exists and may also need to modify nav.path.
 ### nav: the navigator object
 ### This function returns True if the moveTarget and/or path is modified and False otherwise
 def mySmooth(nav):
-	### YOUR CODE GOES BELOW HERE ###
+    
+    ### YOUR CODE GOES BELOW HERE ###
+    if nav.agent.position and nav.destination and ClearShot(nav.agent.position, nav.destination, nav.world.getLines(), nav.world.getPoints(), nav.agent)==True:
+        if not nav.agent.moveToTarget(nav.destination):
+            #nav.agent.stopMoving()
+            pass
+        else:
+            nav.agent.moveToTarget(nav.destination)
+        return True
+    ### YOUR CODE GOES ABOVE HERE ###
+    
+    return False
 
-	### YOUR CODE GOES ABOVE HERE ###
-	return False
+def ClearShot(p1, p2, worldLines, worldPoints, agent):
 
-
+    ### YOUR CODE GOES BELOW HERE ###
+    z=[] 
+    radius=1.1*agent.getMaxRadius()
+    for point in worldPoints:
+        z.append(minimumDistance((p1,p2),point))
+    if rayTraceWorld(p1,p2,worldLines)==None and min(z)>radius:
+        return True
+    ### YOUR CODE GOES ABOVE HERE ###
+    return False
